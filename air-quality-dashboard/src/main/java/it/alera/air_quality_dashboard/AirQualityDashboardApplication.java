ea4b3ca6a49c0c4e6fbaafd5d32b2b6edd764a2e
asnow <ale.raggetti@libero.it>
2025-11-05 22:56:49 +0100

    inizializzazione progetto Spring Boot

---
+++ air-quality-dashboard/src/main/java/it/alera/air_quality_dashboard/AirQualityDashboardApplication.java
@@ -0,0 +1,13 @@
+package it.alera.air_quality_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirQualityDashboardApplication.class, args);
+	}
+
+}
